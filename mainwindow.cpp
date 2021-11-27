3af822523916d3b6bd2f72fcfb0e0219316db2fc
zLucas <duo_lin0119@163.com>
2021-11-27 19:25:13 +0800

    完成主窗口居中显示功能，适用于所有屏幕

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,20 @@
 #include "mainwindow.h"
+#include "QDebug"
+#include <QScreen>
+#include "QGuiApplication"
+#include <QRect>
+
+#define MAINWINDOW_WIDTH  480
+#define MAINWINDOW_HEIGHT 320
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    QList<QScreen *> list_screen =  QGuiApplication::screens();  /*多显示器*/
+    QRect rect = list_screen.at(0)->geometry();
+    this->setGeometry((rect.width() - MAINWINDOW_WIDTH)/2,
+                      (rect.height() - MAINWINDOW_HEIGHT)/2,
+                      MAINWINDOW_WIDTH,MAINWINDOW_HEIGHT);
 }
 
 MainWindow::~MainWindow()
